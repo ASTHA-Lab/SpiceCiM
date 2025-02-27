c958045c66e583c5312355bf6125d4f09356404d
ASTHA-Lab <astha.lab2022@gmail.com>
2025-02-27 16:38:22 -0600

    Update checkInfer.py

--- checkInfer.py
+++ checkInfer.py
@@ -1,107 +1,147 @@
-import os, json
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from scipy.ndimage import zoom
+from PIL import Image
+import pandas as pd
+import math
+import os
+import shutil
+import subprocess
+import shlex
+import csv
+import re
+from supports.rapsody import *
+import sys
+import configparser
 
-def read(path):
-    with open(path, "r") as f:
-        return f.readlines()
+#os.makedirs('tmp')
 
+# Load configuration from config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-filePath="./tmp/output.csv"
-fileDataList=list(read(filePath))
-unitDiv=1e-6
-refValueFile="./tmp/tensorOut.csv"
-refFileData=list(read(refValueFile))
+# Architecture Parameters
+synaptic_array_size = tuple(map(int, config['ARCHITECTURE']['synaptic_array_size'].split(',')))
+pe_size = tuple(map(int, config['ARCHITECTURE']['pe_size'].split(',')))
+tile_size = tuple(map(int, config['ARCHITECTURE']['tile_size'].split(',')))
 
-headerStr=fileDataList.pop(0)
-headerList=headerStr.strip().split(",")
-headerList.pop(0)
-print(headerList)
+# Hardware Constants
+Tox = float(config['HARDWARE']['Tox'])
+Tref = float(config['HARDWARE']['Tref'])
+Ik1 = float(config['HARDWARE']['Ik1'])
+Vref = float(config['HARDWARE']['Vref'])
+alpha = Ik1 / Vref
 
-dataDict={}
+# Paths
+golden_input_path = config['PATHS']['golden_input_path']
+base_dir = config['PATHS']['base_dir']
 
-for line in fileDataList:
-    lineSplit=line.strip().split(",\"")
-    lineHeader=lineSplit.pop(0)
-    row=lineHeader.split("_")[1]
-    col=lineHeader.split("_")[2]
-    dataDictKey=row+"_"+col
-    if not dataDictKey in dataDict:
-        dataDict[dataDictKey]={}
-    sign=lineHeader.split("_")[3]
-    print(sign)
-    count0=0
-    dict0={}
-    for part in lineSplit:
-        y=part.strip("\"")
-        dict0[headerList[count0]]=eval(y)
-        count0+=1
-    dataDict[dataDictKey][sign]=dict0
+# Hardware Parameters
+Tmax = float(config['HARDWARE']['Tmax'])
+Tmin = float(config['HARDWARE']['Tmin'])
+Gmax, Gmin = conductance_calc(alpha, Tox, Tref, Tmax, Tmin)
 
-with open("./tmp/result.json", 'w') as fout:
-    json_dumps_str = json.dumps(dataDict, indent=4)
-    print(json_dumps_str, file=fout)
+# Wire Parasitics
+res_val = float(config['HARDWARE']['res_val'])
+cap_val = float(config['HARDWARE']['cap_val'])
 
-dataDict1={}
+# Simulation Parameters
+PW = float(config['SIMULATION']['PW'])
+Trise = float(config['SIMULATION']['Trise'])
+Tfall = float(config['SIMULATION']['Tfall'])
 
-for array in dataDict:
-    arrayData=dataDict[array]
-    for sign in arrayData:
-        if sign=="pos":
-            mult=1
-        elif sign=="neg":
-            mult=-1
-        signData=arrayData[sign]
-        for Icol in signData:
-            IcolData=signData[Icol]
-            count1=0
-            for I in IcolData:
-                processName="P"+str(count1)
-                if not processName in dataDict1:
-                    dataDict1[processName]={}
-                if not Icol in dataDict1[processName]:
-                    dataDict1[processName][Icol]={
-                                                    "dataList":[],
-                                                    "dataSum":0
-                                                    }
-                calc=(mult*I)/unitDiv
-                dataDict1[processName][Icol]["dataList"].append(calc)
-                dataDict1[processName][Icol]["dataSum"]+=calc
-                count1+=1
+# Inference Parameters
+image_path = config['INFERENCE']['image_path']
+size = tuple(map(int, config['INFERENCE']['size'].split(',')))
+vmax = float(config['INFERENCE']['vmax'])
 
-dataDict2={}
-dataWrite=open("InferenceCheck_Output.csv", "w")
-dataWrite.write("Original,Simulation,Check\n")
-count2=0
-passCount=0
-failCount=0
-for process in dataDict1:
-    maxValue=0
-    maxKey=""
-    processData=dataDict1[process]
-    for data in processData:
-        if processData[data]["dataSum"]>maxValue:
-            maxValue=processData[data]["dataSum"]
-            maxKey=data
-    dataDict2[process]={
-                        "key":maxKey,
-                        "value":maxValue
-                        }
-    refSoftInferData=refFileData[count2].strip().split(",")[0].strip()
-    simulatedData=maxKey.strip().replace("IPRB","").replace(":in","")
-    if refSoftInferData==simulatedData:
-        check="PASS"
-        passCount+=1
-    else:
-        check="FAIL"
-        failCount+=1
-    dataWrite.write(refSoftInferData+","+simulatedData+","+check+"\n")
-    count2+=1
-dataWrite.write("\n\nPASS "+str(passCount)+"\nFAIL "+str(failCount))
-dataWrite.close()
+# Ensure distribution directory is set up
+def setup_distribution_directory(base_dir):
+    if os.path.exists(base_dir):
+        shutil.rmtree(base_dir)
+        #shutil.rmtree('tmp')
+    os.makedirs(base_dir)
+setup_distribution_directory(base_dir)
+setup_distribution_directory('tmp')
 
-with open("./tmp/result2.json", 'w') as fout:
-    json_dumps_str = json.dumps(dataDict1, indent=4)
-    print(json_dumps_str, file=fout)
+def main():
+    #alpha = Ik1/Vref
+    num_epoch = int(config['TRAINING']['num_epoch'])
+    inputlen_sqrt = int(config['TRAINING']['inputlen_sqrt'])
+    outputlen = int(config['TRAINING']['outputlen'])
+    num_hidden_layer = int(config['TRAINING']['num_hidden_layer'])
+    qbit = int(config['TRAINING']['qbit'])
+    
+    model, layer_weights, biases, hardware_reqs = create_and_train_ann_model(qbit, num_epoch, inputlen_sqrt, outputlen, num_hidden_layer, 16)
+    binary_image, inf_image = load_and_process_to_bin_img(image_path, size, vmax, model)
+    
+    total_rows, sim_time = generate_pwl_sources('./tmp/img_to_voltage_data.csv', PW, Trise, Tfall, synaptic_array_size[0])
+    
+    stdcell_file_path = config['PATHS']['stdcell_file_path']
+    top_netlist = generate_subarray(synaptic_array_size[0], synaptic_array_size[1], stdcell_file_path)
+    with open("./tmp/top_netlist.scs", 'w') as file:
+        file.write(top_netlist)
+    
+    for layer_idx, weights in layer_weights.items():
+        distribute_weights({f"{layer_idx}": weights}, {f"{layer_idx}": hardware_reqs[f"{layer_idx}"]})
+    
+    baseline_file_path = config['PATHS']['envm_model_path']
+    
+
+    
+    base_dir = "weights_distribution"
+    
+    for layer_idx in sorted(layer_weights.keys(), key=lambda x: int(x.replace('Layer', ''))):
+        layer_dir = os.path.join("weights_distribution", layer_idx)
+        
+        for root, dirs, files in os.walk(layer_dir, topdown=True):
+            dirs.sort(key=lambda x: int(re.findall(r'\d+', x)[0]))
+            files.sort(key=lambda x: int(re.findall(r'\d+', x.split('_')[1])[0]))
             
-with open("./tmp/result3.json", 'w') as fout:
-    json_dumps_str = json.dumps(dataDict2, indent=4)
-    print(json_dumps_str, file=fout)
+            for file in files:
+                if file.endswith(".npy"):
+                    full_path = os.path.join(root, file)
+                    quantized_weights = np.load(full_path)
+                    Tpos, Tneg = convert_to_conductance(quantized_weights, Gmax, Gmin, Tmin, qbit)
+                    weight_mapping(Tpos, Tneg, baseline_file_path, res_val, cap_val)
+                    infile_noext = os.path.splitext(full_path)[0]
+                    
+                    siminput_path_pos = os.path.join(os.getcwd(), "./tmp/input_pos.scs")
+                    siminput_path_neg = os.path.join(os.getcwd(), "./tmp/input_neg.scs")
+                    
+                    '''if int(layer_idx.replace('Layer', '')) == 1:
+                        run_string = setup_simulation_args(siminput_path_pos, infile_noext, '_pos')
+                        map_primary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)
+                    else:
+                        run_string = setup_simulation_args(siminput_path_pos, infile_noext, '_pos')
+                        map_secondary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)'''
+                        
+                    if int(layer_idx.replace('Layer', '')) == 1:
+                        print(f"Layer-{layer_idx.replace('Layer', '')} Simulation: Positive")
+                        run_string=setup_simulation_args(siminput_path_pos, infile_noext, '_pos')
+                        map_primary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)
+
+                        print(f"Layer-{layer_idx.replace('Layer', '')} Simulation: Negative")
+                        run_string=setup_simulation_args(siminput_path_neg, infile_noext, '_neg')
+                        map_primary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)
+                        #collect_data(infile_noext, cyc_time, total_rows)
+                    else:
+                        print(f"Layer-{layer_idx.replace('Layer', '')} Simulation: Positive")
+                        run_string=setup_simulation_args(siminput_path_pos, infile_noext, '_pos')
+                        map_secondary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)
+
+                        print(f"Layer-{layer_idx.replace('Layer', '')} Simulation: Negative")
+                        run_string=setup_simulation_args(siminput_path_neg, infile_noext, '_neg')
+                        map_secondary_input(siminput_path_pos, siminput_path_neg, full_path, sim_time)
+                        run_sim(run_string, infile_noext)
+
+if __name__ == "__main__":
+    main()
